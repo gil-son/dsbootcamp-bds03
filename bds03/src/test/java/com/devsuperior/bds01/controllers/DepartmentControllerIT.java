50a8eb5a977c59a5a39dacd536ad022a954af8b2
“gil-son” <gilszon@gmail.com>
2021-11-18 14:01:44 -0300

    Testing the Token

--- bds03/src/test/java/com/devsuperior/bds01/controllers/DepartmentControllerIT.java
+++ bds03/src/test/java/com/devsuperior/bds01/controllers/DepartmentControllerIT.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +21,29 @@ public class DepartmentControllerIT {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	private String operatorUsername;
+	private String operatorPassword;
+	private String adminUsername;
+	private String adminPassword;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		operatorUsername = "ana@gmail.com";
+		operatorPassword = "123456";
+		adminUsername = "bob@gmail.com";
+		adminPassword = "123456";
+	}
+	
+	
+	
 	
 	@Test
 	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
