9eb5d1e30a81a5e92d46372c37ea5e5eae462ac1
“gil-son” <gilszon@gmail.com>
2021-11-25 07:23:02 -0300

    Configured test with status

--- bds03/src/test/java/com/devsuperior/bds01/controllers/EmployeeControllerIT.java
+++ bds03/src/test/java/com/devsuperior/bds01/controllers/EmployeeControllerIT.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds03.dto.EmployeeDTO;
+import com.devsuperior.bds03.tests.TokenUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -28,36 +30,55 @@ public class EmployeeControllerIT {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
-	@Test
-	public void findAllShouldReturnPagedResourcesSortedByName() throws Exception {
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	
+	private String operatorUsername;
+	private String operatorPassword;
+	private String adminUsername;
+	private String adminPassword;
+	
+	@BeforeEach
+	void setUp() throws Exception {
 		
-		ResultActions result =
-				mockMvc.perform(get("/employees")
-					.contentType(MediaType.APPLICATION_JSON));
+		operatorUsername = "ana@gmail.com";
+		operatorPassword = "123456";
+		adminUsername = "bob@gmail.com";
+		adminPassword = "123456";
+	}
+	
+	
+	@Test
+	public void insertShouldReturn403WhenOperatorLogged() throws Exception {
+		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+		
+		EmployeeDTO dto = new EmployeeDTO(null, "Joaquim", "joaquim@gmail.com", 1L);
+		String jsonBody = objectMapper.writeValueAsString(dto);
 
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$.content").exists());
-		result.andExpect(jsonPath("$.content[0].name").value("Alex"));
-		result.andExpect(jsonPath("$.content[1].name").value("Ana"));
-		result.andExpect(jsonPath("$.content[2].name").value("Andressa"));
+		ResultActions result =
+				mockMvc.perform(post("/employees")
+					.header("Authirization", "Barrer " + accessToken)
+					.content(jsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isForbidden());
 	}
 	
 	@Test
-	public void insertShouldInsertResource() throws Exception {
-
+	public void insertShouldReturn401WhenNoUserLogged() throws Exception {
+		
 		EmployeeDTO dto = new EmployeeDTO(null, "Joaquim", "joaquim@gmail.com", 1L);
 		String jsonBody = objectMapper.writeValueAsString(dto);
-		
+
 		ResultActions result =
 				mockMvc.perform(post("/employees")
 					.content(jsonBody)
 					.contentType(MediaType.APPLICATION_JSON)
 					.accept(MediaType.APPLICATION_JSON));
 		
-		result.andExpect(status().isCreated());
-		result.andExpect(jsonPath("$.id").exists());
-		result.andExpect(jsonPath("$.name").value("Joaquim"));
-		result.andExpect(jsonPath("$.email").value("joaquim@gmail.com"));
-		result.andExpect(jsonPath("$.departmentId").value(1L));
+		result.andExpect(status().isUnauthorized());
 	}	
 }
