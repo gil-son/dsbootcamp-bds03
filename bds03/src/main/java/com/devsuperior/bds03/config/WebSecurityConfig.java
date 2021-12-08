f593ae5eee17a81dcdd7416134e709b42ae18ac7
“gil-son” <gilszon@gmail.com>
2021-12-07 21:45:41 -0300

    Preparing security applications

--- bds03/src/main/java/com/devsuperior/bds03/config/WebSecurityConfig.java
+++ bds03/src/main/java/com/devsuperior/bds03/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.devsuperior.bds03.config;
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+	
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/actuator/**"); // ignore any endpoint and pass between actuator (library from Spring OAuth)
+		web.ignoring().antMatchers("/**"); // ignore any endpoint	
 	}
-
 	
 }
